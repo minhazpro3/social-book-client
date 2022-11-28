cc303bf10e8cd339aea738b6ada501fad3f4d70f
minhazpro3 <minhazpro1@gmail.com>
2022-11-28 20:51:01 +0600

    login and register page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,55 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 export const Login = () => {
-  return <div>Login</div>;
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
+  return (
+    <div className="container mt-5 ">
+      <div className="d-flex justify-content-center">
+        <div
+          className="my-5  d-inline-block rounded-top"
+          style={{ backgroundColor: "#8080ff" }}
+        >
+          <h2 className="text-white py-4 px-3">Please Login !!!</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="py-4 px-3">
+              <input
+                className="w-100  rounded-pill px-2 border-0 py-1 form-control bg-muted "
+                {...register("email")}
+                required
+                placeholder="E-mail"
+                type="email"
+              />
+              <br />
+
+              <input
+                className=" w-100 my-2 rounded-pill px-2 border-0 py-1 form-control bg-muted "
+                {...register("password")}
+                required
+                placeholder="Password"
+                type="password"
+              />
+              <br />
+
+              <input
+                style={{ backgroundColor: "#7733ff" }}
+                className="w-100  text-white rounded-pill px-2 border-0 py-1 form-control bg-muted"
+                type="submit"
+              />
+            </div>
+          </form>
+          <div className="d-flex justify-content-center">
+            {" "}
+            <p className="text-white">
+              Don't have an account. <Link to="register">Register</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
   return (
-    <div>Register</div>
-  )
-}
+    <div className="container  ">
+      <div className="d-flex justify-content-center">
+        <div
+          className="my-5  d-inline-block rounded-top"
+          style={{ backgroundColor: "#8080ff" }}
+        >
+          <h2 className="text-white py-4 px-3">Register Now !!!</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="py-4 px-3">
+              <input
+                className="w-100  rounded-pill px-2 border-0 py-1 form-control bg-muted "
+                {...register("name")}
+                required
+                placeholder="User name"
+                type="text"
+              />
+              <br />
+              <input
+                className="w-100 mt-2 rounded-pill px-2 border-0 py-1 form-control bg-muted "
+                {...register("email")}
+                required
+                placeholder="E-mail"
+                type="email"
+              />
+              <br />
 
-export default Register
+              <input
+                className=" w-100 my-2 rounded-pill px-2 border-0 py-1 form-control bg-muted "
+                {...register("password")}
+                required
+                placeholder="Password"
+                type="password"
+              />
+              <br />
+              <input
+                className=" w-100  rounded-pill px-2 border-0 py-1 form-control bg-muted "
+                {...register("password2")}
+                required
+                placeholder="Confirm Password"
+                type="password"
+              />
+              <br />
+
+              <input
+                style={{ backgroundColor: "#7733ff" }}
+                className="w-100  text-white rounded-pill px-2 border-0 py-1 form-control bg-muted"
+                type="submit"
+              />
+            </div>
+          </form>
+          <div className="d-flex justify-content-center">
+            {" "}
+            <p className="text-white">
+              Do you have an account. <Link to="/">Login</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
